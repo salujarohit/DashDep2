e442dc68dba7b6b6040ed3d43b4eb25922e09091
Rohit Saluja <salujarohit@outlook.com>
2020-05-09 15:11:44 +0300

    main file update

--- app.py
+++ app.py
@@ -28,31 +28,34 @@ def main():
 	choices = st.sidebar.selectbox("Options", menu)
 
 	if choices == 'Revenue Forecasting':
-		st.subheader("Revenue Forecasting using Last Month's Activity")
+		st.subheader("Revenue Forecasting using Sales Energy")
 
-		meetings_sales_competence = st.number_input("Meetings Organized by Futuricians with Sales Competence")
-		meeting_tc_sales = st.number_input("Meetings Sales Competence with Top Clients ")
-		meeting_regarding_TC = st.number_input("Meetings Regarding Top Clients")
-		client_meeting_energy = st.number_input("M2 Recipts : Client Negotiations")
-		taxi_energy = st.number_input("M2 Recipts : Taxi ")
-		travel_parking_energy = st.number_input("M2 Recipts : Travel and Parking")
+		month_choice = st.selectbox("Select the Amount of Sales Energy for Forecasting", ["Previous Month","Previous 2 Months","Previous 3 Months"])
+		if month_choice == "Previous Month":
 
-		sample_data = [meetings_sales_competence,meeting_tc_sales,meeting_regarding_TC,client_meeting_energy,taxi_energy,travel_parking_energy]
+			meetings_sales_competence = st.number_input("Meetings Organized by Futuricians with Sales Competence")
+			meeting_tc_sales = st.number_input("Meetings Sales Competence with Top Clients ")
+			meeting_regarding_TC = st.number_input("Meetings Regarding Top Clients")
+			client_meeting_energy = st.number_input("M2 Recipts : Client Negotiations")
+			taxi_energy = st.number_input("M2 Recipts : Taxi ")
+			travel_parking_energy = st.number_input("M2 Recipts : Travel and Parking")
 
-		pred_data = np.array(sample_data).reshape(1,-1)
+			sample_data = [meetings_sales_competence,meeting_tc_sales,meeting_regarding_TC,client_meeting_energy,taxi_energy,travel_parking_energy]
+
+			pred_data = np.array(sample_data).reshape(1,-1)
 
 
-		#model_choice = st.selectbox("Make Prediction using Mothly Activity",["Last Month's Activity"])
-		
-		if st.button("Predict"):
-			#if model_choice == "Last Month's Activity":
-			predictor = load_prediction_model("svr_t6.pkl")
-			prediction = predictor.predict(pred_data)
-			st.write(prediction)
+			#model_choice = st.selectbox("Make Prediction using Mothly Activity",["Last Month's Activity"])
 			
+			if st.button("Predict"):
+				#if model_choice == "Last Month's Activity":
+				predictor = load_prediction_model("svr_t6.pkl")
+				prediction = predictor.predict(pred_data)
+				#st.write(prediction)
+				
 
-		#final_result = prediction
-		#st.success("Predicted Revenie after six months",prediction)
+				final_result = "{:.2f}".format(float(prediction))
+				st.success(final_result)
 
 
 
